a781ce8e762d2d759089c73987ea539e4e93db6c
laney522 <pm72222@gmail.com>
2024-06-17 16:37:35 -0400

    frontend files update-add navbar light mode

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Container, Stack } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <Stack minH={"100vh"}>
+      <Navbar />
+      <Container maxW={"1200px"} my={4}>
+
+      </Container>
+    </Stack>
+  )
+}
+
+export default App;
